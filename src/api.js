0f00886aea2c2278976a158b25169cac1213f31c
carlos.franca <carlos.lopes@fiskal.com.br>
2024-05-31 17:26:59 -0300

    Atualizando Url

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
     //baseURL: 'http://localhost:3004/',
-    baseURL: 'https://pet-solutions-backend-rgtl8eose-carlos-juniors-projects.vercel.app',
+    baseURL: 'https://pet-solutions-frontend-v95l.vercel.app/',
     headers : {
         'Content-Type': 'application/json',
         'Cache-Control': 'no-cache',
