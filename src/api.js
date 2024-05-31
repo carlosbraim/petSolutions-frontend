495ce84c4a4baec681d7d5fae19b0d6e9f0747b9
carlos.franca <carlos.lopes@fiskal.com.br>
2024-05-31 17:37:16 -0300

    url backend

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
     //baseURL: 'http://localhost:3004/',
-    baseURL: 'https://pet-solutions-frontend-v95l.vercel.app/',
+    baseURL: 'pet-solutions-backend.vercel.app',
     headers : {
         'Content-Type': 'application/json',
         'Cache-Control': 'no-cache',
